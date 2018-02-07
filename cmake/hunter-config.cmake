6f7d9de8497ab56346ad0f71d1bb3ae5d6d0cf9c
Bogdan Vaneev <warchantua@gmail.com>
2018-02-07 23:58:45 +0300

    Add gtest
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

--- cmake/hunter-config.cmake
+++ cmake/hunter-config.cmake
@@ -1,8 +1,8 @@
 # gtest
-hunter_config(GTest VERSION "1.8.0-hunter-p11"
+hunter_config(GTest
+    VERSION
+      1.8.0-hunter-p11
     CMAKE_ARGS
-    -Dgtest_force_shared_crt=ON
-    -Dgtest_disable_pthreads=OFF
+      -Dgtest_force_shared_crt=ON
+      -Dgtest_disable_pthreads=OFF
     )
-
-
