ee062ebdae6c678c8a9787aa224b7200b2a90338
luckychess <toobwn@gmail.com>
2018-03-20 12:47:41 +0300

    Add set -ex; remove nonstd::optional; use Verbose
    Signed-off-by: luckychess <toobwn@gmail.com>

--- example/java/Android/android-build.sh
+++ example/java/Android/android-build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -ex
 PLATFORM=$1
 NDK_PATH=$2
 PACKAGE=$3
@@ -27,22 +28,17 @@ cp -R ./boost_1_66_0/boost ${DEPS_DIR}/include
 git clone https://github.com/google/protobuf
 (cd ./protobuf ; git checkout b5fbb742af122b565925987e65c08957739976a7)
 cmake -Dprotobuf_BUILD_TESTS=OFF -H./protobuf/cmake -B./protobuf/host_build # build for host to get js_embed
-cmake --build ./protobuf/host_build
+VERBOSE=1 cmake --build ./protobuf/host_build
 sed -i.bak "s~COMMAND js_embed~COMMAND $(pwd)/protobuf/host_build/js_embed~" ./protobuf/cmake/libprotoc.cmake
 LDFLAGS="-llog -landroid" cmake ${ANDROID_TOOLCHAIN_ARGS} ${INSTALL_ARGS} -Dprotobuf_BUILD_TESTS=OFF -H./protobuf/cmake -B./protobuf/.build
-cmake --build ./protobuf/.build --target install
+VERBOSE=1 cmake --build ./protobuf/.build --target install
 
 # ed25519
 git clone git://github.com/hyperledger/iroha-ed25519
 (cd ./iroha-ed25519 ; git checkout e7188b8393dbe5ac54378610d53630bd4a180038)
 cmake ${ANDROID_TOOLCHAIN_ARGS} ${INSTALL_ARGS} -DTESTING=OFF -DBUILD=STATIC -H./iroha-ed25519 -B./iroha-ed25519/build
-cmake --build ./iroha-ed25519/build --target install
+VERBOSE=1 cmake --build ./iroha-ed25519/build --target install
 mv ${DEPS_DIR}/lib/static/libed25519.a ${DEPS_DIR}/lib; rmdir ${DEPS_DIR}/lib/static/
-
-# nonstd-optional
-git clone https://github.com/martinmoene/optional-lite
-(cd ./optional-lite ; git checkout a0ddabb8b52e1eaaf0dd1515bb85698b747528e4)
-cp -R ./optional-lite/include/nonstd ${DEPS_DIR}/include
 
 # SWIG fixes
 sed -i.bak "s~find_package(JNI REQUIRED)~#find_package(JNI REQUIRED)~" ./iroha/shared_model/bindings/CMakeLists.txt
@@ -54,7 +50,7 @@ sed -i.bak "s~swig_link_libraries(irohajava~swig_link_libraries(irohajava $(pwd)
 # build iroha
 sed -i.bak "s~find_program(protoc_EXECUTABLE protoc~set(protoc_EXECUTABLE $(pwd)/protobuf/host_build/protoc~" ./iroha/cmake/Modules/Findprotobuf.cmake # use host protoc
 cmake -H./iroha/shared_model -B./iroha/shared_model/build ${ANDROID_TOOLCHAIN_ARGS} -DTESTING=OFF -DSHARED_MODEL_DISABLE_COMPATIBILITY=ON -DSWIG_JAVA=ON -DCMAKE_PREFIX_PATH=${DEPS_DIR}
-cmake --build ./iroha/shared_model/build --target irohajava
+VERBOSE=1 cmake --build ./iroha/shared_model/build --target irohajava
 
 # copy artifacts
 mkdir lib
